f257d4554fc966f1b2299917aeaeea5c162b4e83
mattew94 <web.matteoromano@gmail.com>
2025-01-08 22:39:59 +0100

    initial commit

---
+++ functions/lib/index.js
@@ -0,0 +1,24 @@
+"use strict";
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.myFunction = void 0;
+const https_1 = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+exports.myFunction = (0, https_1.onRequest)((req, res) => {
+    logger.log("Function triggered");
+    res.send("Hello from Firebase!");
+});
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+//# sourceMappingURL=index.js.map
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideHttpClient() /* TODO: poi aggiungere gli interceptor al suo interno -> withInterceptors([httpInterceptor]) */,
+    provideRouter(routes),
+    /* 
+    TODO: aggiungere quando creerò lo store
+    provideStore(),
+    provideEffects([]), 
+    */
+  ],
 };
